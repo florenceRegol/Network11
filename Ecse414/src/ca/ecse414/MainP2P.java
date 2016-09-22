6aa1c74c08c9a5e92f803fc38dc9fdd9cbf32efd
floregol <florence.robert-regol@mail.mcgill.ca>
2016-09-21 21:36:24 -0400

    pull test

--- Ecse414/src/ca/ecse414/MainP2P.java
+++ Ecse414/src/ca/ecse414/MainP2P.java
@@ -3,7 +3,7 @@ package ca.ecse414;
 public class MainP2P {
 
 	public static void main(String[] args) {
-		System.out.println("chillos potatos");;
+		System.out.println("chillos potatos2");;
 
 	}
 
